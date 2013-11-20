3321aa03467765cdbdce09569f9dd8de66307a91
Bruno Alano <bruno@appzlab.com>
2013-11-20 16:42:50 -0200

    Adopted the new library format
    We are using now the new (beta) library format of Arduino Libraries

---
+++ src/cppduino.h
@@ -0,0 +1,56 @@
+/**
+ * cppduino - Arduino C++ Library
+ * Copyright (C) 2013 Bruno Medina. All rights reserved.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+// Guard
+#ifndef CPPDUINO_SRC_CPPDUINO_H
+#define CPPDUINO_SRC_CPPDUINO_H
+
+// Project Dependencies
+
+/**
+ * cppduino namespace
+ *
+ * The project use some conventions based on the Google C++ Convention,
+ * where all the methods, classes, enums, are in the cppduino namespace.
+ */
+namespace cppduino {
+  /**
+   * Arduino Models
+   *
+   * Alocamos as versão suportadas do Arduino onde no futuro
+   * poderemos utilizar
+   */
+  namespace models {
+    enum
+    {
+      ARDUINO_MODEL_UNO
+    };
+  }
+
+  /**
+   * Arduino Mode
+   *
+   * Modos de saídas das portas digitais, podendo
+   * ser ligadas ou desligadas
+   */
+  namespace output {
+    enum
+    {
+      ON = OUTPUT,
+      OFF = INPUT
+    };
+  }
+}
+
+// End Guard
+#endif
